000054fbff4d259b1a579e166deb1869e70faa1c
Justinsandy99 <justinsandy080@gmail.com>
2025-12-02 16:53:26 -0500

    Creation de la class produit dans la page produit

---
+++ lib/Pages/produit.dart
@@ -0,0 +1,13 @@
+class Produit {
+  final String nom;
+  final String description;
+  final double prix;
+  final String image;
+
+  Produit({
+    required this.nom,
+    required this.description,
+    required this.prix,
+    required this.image,
+  });
+}
