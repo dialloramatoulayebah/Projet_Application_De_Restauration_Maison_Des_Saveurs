3e0af12c41e3032c7580df5a4170a473c00fb13c
Justinsandy99 <justinsandy080@gmail.com>
2025-12-03 14:53:11 -0500

    Repas

--- lib/Pages/menu.dart
+++ lib/Pages/menu.dart
@@ -12,26 +12,76 @@ class ListProduits extends StatelessWidget {
     Salade(
       nom: "Salade César",
       description: "Salade fraîche avec poulet grillé et parmesan.",
-      prix: 12.0,
+      prix: 12.00,
       image: "assets/images/salades/saladesoeufs.jpeg",
     ),
     Salade(
       nom: "Salade Fruits",
       description: "Mélange de fruits frais.",
-      prix: 9.0,
+      prix: 9.00,
       image: "assets/images/salades/Saladelegumes.jpg",
     ),
     Salade(
       nom: "Salade Créole",
       description: "Salade épicée façon créole.",
-      prix: 14.0,
+      prix: 14.00,
       image: "assets/images/salades/salades3.jpg",
     ),
     Salade(
       nom: "Salade poulet braisé",
       description: "Salade épicée façon créole.",
-      prix: 7.0,
+      prix: 7.00,
       image: "assets/images/salades/salades12.jpg",
+    ),
+  ];
+    final List<Repas> repas = [
+    Repas(
+      nom: "Alloco Poisson",
+      description: "Poisson grillé accompagné d’alloco (bananes plantains frites), servi avec une petite sauce maison.",
+      prix: 18.00,
+      image: "assets/images/Repas/allocopoisson.jpeg",
+    ),
+    Repas(
+      nom: "Attiéké Poisson",
+      description: "Attiéké ivoirien accompagné d’un poisson braisé, tomate, oignon, piment et sauce verte traditionnelle.",
+      prix: 17.00,
+      image: "assets/images/Repas/attiekepoisssson.jpg",
+    ),
+    Repas(
+      nom: "Tajine Marocain aux Pruneaux",
+      description: "Tajine marocain traditionnel à la viande, pruneaux, amandes et épices orientales.",
+      prix: 20.00,
+      image: "assets/images/Repas/tajine-marocain-aux-pruneaux.jpg",
+    ),
+    Repas(
+      nom: "Tajine Marocain Traditionnel",
+      description: "Tajine aux légumes variés, épices marocaines et viande tendre cuite lentement.",
+      prix: 18.00,
+      image: "assets/images/Repas/salades12.jpg",
+    ),
+    Repas(
+      nom: "Brochettes Grillées",
+      description: "Brochettes de viande marinée, grillées au feu et servies avec une sauce épicée.",
+      prix: 15.00,
+      image: "assets/images/Repas/Brochette.jpeg",
+    ),
+    Repas(
+      nom: "Riz Sauce Arachide",
+      description: "Riz blanc accompagné d’une sauce onctueuse à base d’arachide et de viande mijotée.",
+      prix: 14.00,
+      image: "assets/images/Repas/rizsaucearachide.jpeg",
+    ),
+    Repas(
+      nom: "chièp (Thieboudienne)",
+      description: "Riz sénégalais parfumé, servi avec poisson, carottes, chou et légumes mijotés.",
+      prix: 19.00,
+      image: "assets/images/Repas/Tchiep.jpg",
+    ),
+    Repas(
+      nom: "Salade poulet braisé",
+      description: "Soupe ou ragoût nourrissant à base de pois chiches, de couscous perlé (Maftoul), de carottes et de courgettes, dans un bouillon épicé à la tomate. Servi avec des quartiers d'œuf dur.",
+      prix: 12.00,
+      image: "assets/images/Repas/soupeauxpoischiche.jpeg",
     ),
   ];
 
@@ -54,7 +104,20 @@ class ListProduits extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: GridView.builder(
+        child: 
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Salades Fraîches",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+
+              ),
+            ),
+            SizedBox(height: 10),
+            GridView.builder(
           itemCount: produits.length,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 4,
@@ -115,6 +178,9 @@ class ListProduits extends StatelessWidget {
             );
           },
         ),
+          ],
+        )
+        
       ),
     );
   }
