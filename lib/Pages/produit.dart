e529fd94a5d75f134a106f46882d6f914fb58fab
Justinsandy99 <justinsandy080@gmail.com>
2025-12-03 16:12:30 -0500

    Le menu

--- lib/Pages/produit.dart
+++ lib/Pages/produit.dart
@@ -1,26 +1,38 @@
-class Salade {
+class Produit {
   final String nom;
   final String description;
   final double prix;
   final String image;
 
-  Salade({
+  Produit({
     required this.nom,
     required this.description,
     required this.prix,
     required this.image,
   });
 }
-class Repas {
-  final String nom;
-  final String description;
-  final double prix;
-  final String image;
 
+class Salade extends Produit {
+  Salade({
+    required super.nom,
+    required super.description,
+    required super.prix,
+    required super.image,
+  });
+}
+class Repas extends Produit {
   Repas({
-    required this.nom,
-    required this.description,
-    required this.prix,
-    required this.image,
+    required super.nom,
+    required super.description,
+    required super.prix,
+    required super.image,
+  });
+}
+class Boisson extends Produit {
+  Boisson({
+    required super.nom,
+    required super.description,
+    required super.prix,
+    required super.image,
   });
 }
